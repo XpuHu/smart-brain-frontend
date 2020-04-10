af5ee7967c103c4a8ebb7fc5d98dca3f23d199bf
XpuHu <ecaterinacovali93@gmail.com>
2020-04-10 13:48:34 +0300

    init commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const Rank = ({ name, entries}) => {
+    return(
+        <Fragment>
+            <div className='white f3'>
+                {`${name}, your current entry count is...`}
+            </div>
+            <div className='white f1'>
+                {`#${entries}`}
+            </div>
+        </Fragment>
+    );
+}
+
+export default Rank;
